18f2c5268c3204143b7c940d7788342e5fb535bb
manh phuc <manhphuc300501@gmail.com>
2026-08-27 22:09:04 +0700

    fix: Cap nhat import BuildConfig dung voi package name moi
    - Thay doi package import cho BuildConfig tuong ung voi package name da cap nhat trong MainActivity.java

--- app/src/main/java/com/nguyenmanhphuc/view2earn/app/MainActivity.java
+++ app/src/main/java/com/nguyenmanhphuc/view2earn/app/MainActivity.java
@@ -26,6 +26,7 @@ import com.nguyenmanhphuc.view2earn.app.models.User;
 import com.nguyenmanhphuc.view2earn.app.services.HttpRequest;
 import com.nguyenmanhphuc.view2earn.app.services.Response;
 import com.nguyenmanhphuc.view2earn.app.utils.SessionManager;
+import com.vuhongcat.view2earn.app.BuildConfig;
 import com.vuhongcat.view2earn.app.R;
 
 import java.text.DecimalFormat;
